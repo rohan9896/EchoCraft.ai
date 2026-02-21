a7dd8027bff3807b12071929a71b8ad5bcb80092
Rohan Gupta <rohang9896@gmail.com>
2026-02-21 16:24:31 +0530

    feat: integrate better-auth for user authentication and add related database models

---
+++ frontend/src/lib/auth.ts
@@ -0,0 +1,13 @@
+import { PrismaClient } from "../../generated/prisma";
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+
+const prisma = new PrismaClient();
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql", // or "mysql", "sqlite", ...etc
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+});
